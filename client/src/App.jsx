cc77224e667b7dcfe3b67782ee505130c6a9a575
Huntercoding02 <138290654+Huntercoding02@users.noreply.github.com>
2024-08-08 10:01:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./components/Home";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <Home />
+  )
+}
+
+export default App;
+
+//<div className="App">{}</div>;
